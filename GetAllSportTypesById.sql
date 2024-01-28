2ca3e21aee255deed95e3287ff86df5ba50f1a24
Василий Мысляев <myslyaev@gmail.com>
2024-01-28 17:10:55 +0300

    Update GetAllSportTypesById

--- GetAllSportTypesById.sql
+++ GetAllSportTypesById.sql
@@ -5,7 +5,7 @@ Create procedure GetAllSportTypesById
 @Id int
 As
 Begin
-Select [Id], [Name] from dbo.[SportTypes]
-Where [Id]=@Id
+Select [Id], [Name] from dbo.[SportTypes] as ST
+Where [Id]=@Id and St.[IsDeleted]=0
 End
 Go
