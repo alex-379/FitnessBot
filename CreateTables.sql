affe0ee36897d2640150b198e8113322d4aefccf
alex-379 <alexey_379@mail.ru>
2024-01-28 17:00:27 +1000

    Change CreateTables

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,125 +1,125 @@
-Create database FitnessBot;
+Create database FitnessBot
 Go
 
-Use FitnessBot;
+Use FitnessBot
 
-Create table Roles
+Create table [Roles]
 (
-	Id int NOT NULL PRIMARY KEY IDENTITY,
+	[Id] int NOT NULL PRIMARY KEY IDENTITY,
 	[Name] nvarchar (20) NOT NULL,
-	IsDeleted bit NOT NULL
-);
+	[IsDeleted] bit NOT NULL
+)
 Go
 
-Create table Persons
+Create table [Persons]
 (
-	Id int NOT NULL PRIMARY KEY IDENTITY,
-	RoleId int NOT NULL,
-	FamilyName nvarchar (20),
-	FirstName nvarchar (20),
-	Patronymic nvarchar (20),
-	PhoneNumber nvarchar (12),
-	Email nvarchar (40),
-	Age int,
-	Sex bit,
-	IsDeleted bit NOT NULL
-);
+	[Id] int NOT NULL PRIMARY KEY IDENTITY,
+	[RoleId] int NOT NULL,
+	[FamilyName] nvarchar (20),
+	[FirstName] nvarchar (20),
+	[Patronymic] nvarchar (20),
+	[PhoneNumber] nvarchar (12),
+	[Email] nvarchar (40),
+	[Age] int,
+	[Sex] bit,
+	[IsDeleted] bit NOT NULL
+)
 Go
 
-Create table WorkoutTypes
+Create table [WorkoutTypes]
 (
-	Id int NOT NULL PRIMARY KEY IDENTITY,
+	[Id] int NOT NULL PRIMARY KEY IDENTITY,
 	[Name] nvarchar (50) NOT NULL,
-	IsDeleted bit NOT NULL
-);
+	[IsDeleted] bit NOT NULL
+)
 Go
 
 Create table SportTypes
 (
-	Id int NOT NULL PRIMARY KEY IDENTITY,
+	[Id] int NOT NULL PRIMARY KEY IDENTITY,
 	[Name] nvarchar (50) NOT NULL,
-	IsDeleted bit NOT NULL
-);
+	[IsDeleted] bit NOT NULL
+)
 Go
 
 Create table Workouts
 (
-	Id int NOT NULL PRIMARY KEY IDENTITY,
-	SportTypeId int NOT NULL,
-	Price decimal,
-	Duration int,
-	NumberPlaces int,
-	IsGroup bit,
-	Comment nvarchar (255),
-	IsDeleted bit NOT NULL
-);
+	[Id] int NOT NULL PRIMARY KEY IDENTITY,
+	[SportTypeId] int NOT NULL,
+	[Price] decimal,
+	[Duration] int,
+	[NumberPlaces] int,
+	[IsGroup] bit,
+	[Comment] nvarchar (255),
+	[IsDeleted] bit NOT NULL
+)
 Go
 
-Create table Gyms
+Create table [Gyms]
 (
-	Id int NOT NULL PRIMARY KEY IDENTITY,
+	[Id] int NOT NULL PRIMARY KEY IDENTITY,
 	[Name] nvarchar (50) NOT NULL,
-	IsDeleted bit NOT NULL
-);
+	[IsDeleted] bit NOT NULL
+)
 Go
 
-Create table Timetables
+Create table [Timetables]
 (
-	Id int NOT NULL PRIMARY KEY IDENTITY,
+	[Id] int NOT NULL PRIMARY KEY IDENTITY,
 	[DateTime] nvarchar (8) NOT NULL,
-	CoachId int NOT NULL,
-	WorkoutId int NOT NULL,
-	GymId int NOT NULL,
-	IsDeleted bit NOT NULL
-);
+	[CoachId] int NOT NULL,
+	[WorkoutId] int NOT NULL,
+	[GymId] int NOT NULL,
+	[IsDeleted] bit NOT NULL
+)
 Go
 
 Create table Coaches_WorkoutTypes
 (
-	CoachId int NOT NULL,
-	WorkoutTypeId int NOT NULL
-);
+	[CoachId] int NOT NULL,
+	[WorkoutTypeId] int NOT NULL
+)
 Go
 
 Create table Coaches_SportTypes
 (
-	CoachId int NOT NULL,
-	SportTypeId int NOT NULL
-);
+	[CoachId] int NOT NULL,
+	[SportTypeId] int NOT NULL
+)
 Go
 
-Create table Clients_Timetables
+Create table [Clients_Timetables]
 (
-	ClientId int NOT NULL,
-	TimetableId int NOT NULL
-);
+	[ClientId] int NOT NULL,
+	[TimetableId] int NOT NULL
+)
 Go
 
-Alter table Persons
-Add FOREIGN KEY(RoleId) REFERENCES Roles (Id);
+Alter table [Persons]
+Add FOREIGN KEY([RoleId]) REFERENCES [Roles] ([Id]);
 Go
 
-Alter table Workouts
-Add FOREIGN KEY(SportTypeId) REFERENCES SportTypes (Id);
+Alter table [Workouts]
+Add FOREIGN KEY([SportTypeId]) REFERENCES [SportTypes] ([Id]);
 Go
 
-Alter table Timetables
-Add FOREIGN KEY(CoachId) REFERENCES Persons (Id),
-	FOREIGN KEY(WorkoutId) REFERENCES Workouts (Id),
-	FOREIGN KEY(GymId) REFERENCES Gyms (Id);
+Alter table [Timetables]
+Add FOREIGN KEY([CoachId]) REFERENCES [Persons] ([Id]),
+	FOREIGN KEY([WorkoutId]) REFERENCES [Workouts] ([Id]),
+	FOREIGN KEY([GymId]) REFERENCES [Gyms] ([Id]);
 Go
 
-Alter table Coaches_WorkoutTypes
-Add FOREIGN KEY(CoachId) REFERENCES Persons (Id),
-	FOREIGN KEY(WorkoutTypeId) REFERENCES WorkoutTypes (Id);
+Alter table [Coaches_WorkoutTypes]
+Add FOREIGN KEY([CoachId]) REFERENCES Persons ([Id]),
+	FOREIGN KEY([WorkoutTypeId]) REFERENCES WorkoutTypes ([Id]);
 Go
 
-Alter table Coaches_SportTypes
-Add FOREIGN KEY(CoachId) REFERENCES Persons (Id),
-	FOREIGN KEY(SportTypeId) REFERENCES SportTypes (Id);
+Alter table [Coaches_SportTypes]
+Add FOREIGN KEY([CoachId]) REFERENCES [Persons] ([Id]),
+	FOREIGN KEY([SportTypeId]) REFERENCES [SportTypes] ([Id]);
 Go
 
-Alter table Clients_Timetables
-Add FOREIGN KEY(ClientId) REFERENCES Persons (Id),
-	FOREIGN KEY(TimetableId) REFERENCES Timetables (Id);
+Alter table [Clients_Timetables]
+Add FOREIGN KEY([ClientId]) REFERENCES [Persons] ([Id]),
+	FOREIGN KEY([TimetableId]) REFERENCES [Timetables] ([Id]);
 Go
