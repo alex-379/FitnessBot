93cccec3c2aa72f51668dc6005af6a01eba888b2
alex-379 <alexey_379@mail.ru>
2024-02-02 01:45:02 +1000

    init

--- FitnessClub/FitnessClub.DAL/DTOs/PersonDTO.cs
+++ FitnessClub/FitnessClub.DAL/DTOs/PersonDTO.cs
@@ -8,6 +8,24 @@ namespace FitnessClub.DAL.DTOs
 {
     public class PersonDTO
     {
+        public int Id { get; set; }
 
+        public int RoleId { get; set; }
+
+        public string? FamilyName { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? Patronymic { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? DateBirth { get; set; }
+
+        public bool? Sex { get; set; }
+
+        public int IsDeleted { get; set; }
     }
 }
