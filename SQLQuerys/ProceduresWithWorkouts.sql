5d31fc058765536830526354b73e7b95e12f4f7a
Dmitriy Dmitriy <dbilica19@gmail.com>
2024-02-03 18:14:02 +0300

    Add Delete and Update by workouts

--- SQLQuerys/ProceduresWithWorkouts.sql
+++ SQLQuerys/ProceduresWithWorkouts.sql
@@ -29,3 +29,30 @@ BEGIN
 	Where [Id]=@Id and [IsDeleted] <> 0
 END
 Go
+
+Create procedure DeleteWorkoutsById  
+	@Id int 
+As 
+BEGIN 
+	Update dbo.[Workouts] 
+	Set  [IsDeleted]=1 
+	Where [Id]=@Id 
+END
+Go 
+
+Create procedure UpdateWorkoutsById
+	@Id int,
+	@SportTypeId int,
+	@Price int NULL,
+	@Duration int NULL,
+	@NumberPlaces int NULL,
+	@IsGroup bit NULL,
+	@Comment nvarchar(250) NULL 
+As
+BEGIN
+	Update dbo.[Workouts]
+	Set [SportTypeId] = @SportTypeId, [Price] = @Price, [Duration] = @Duration, [NumberPlaces] = @NumberPlaces, 
+	[IsGroup] = @IsGroup, [Comment] = @Comment
+	Where [Id]=@Id and [IsDeleted]=0
+END
+Go
