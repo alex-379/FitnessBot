bb1a224922fc2229074878c4a8f751faad4f404f
alex-379 <alexey_379@mail.ru>
2024-02-04 21:05:50 +1000

    Update PersonDTO

---
+++ FitnessClub/FitnessClub.DAL/StoredProcedures/PersonStoredProcedures.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.DAL.StoredProcedures
+{
+    public class PersonStoredProcedures
+    {
+        public const string AddPerson = "AddPerson";
+        public const string GetAllPersons= "GetAllPersons";
+        public const string GetPersonById = "GetPersonById";
+    }
+}
