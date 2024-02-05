edebedf96c11919b85c02d1d461cdb0b32af82c9
Василий Мысляев <myslyaev@gmail.com>
2024-02-05 03:17:55 +0300

    Add TimetableDTO
    Add WorkoutDTO
    Add ITimetableRepository
    Add TimetableStoredProcedures
    Add TimetableRepository

---
+++ FitnessClub/FitnessClub.DAL/DTOs/TimetableDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.DAL.DTOs
+{
+    public class TimetableDTO
+    {
+        public int? Id { get; set; }
+        public string DateTime { get; set; }
+        public int CoachId { get; set; }
+        public int WorkoutId {  get; set; }
+        public int GymId { get; set; }
+        public WorkoutDTO Workout { get; set; }
+        public int? ClientId { get; set;}
+        public int? TimetableId { get; set;}
+
+    }
+}
