01d889519a7346c9338c4a32c46c326998dee7de
Василий Мысляев <myslyaev@gmail.com>
2024-02-08 00:39:16 +0300

    Update GymDTO
    Update RoleDTO
    Update SportTypeDTO
    Update TimetableDTO
    Update WorkoutDTO
    Update WorkoutTypeDTO
    Update ITimetableRepository
    Update TimetableStoredProcedures
    Update TimetableRepository
    Update FitnessClub.sln
    Update ProceduresWithTimetables

--- FitnessClub/FitnessClub.DAL/DTOs/SportTypeDto.cs
+++ FitnessClub/FitnessClub.DAL/DTOs/SportTypeDto.cs
@@ -2,8 +2,8 @@
 {
     public class SportTypeDto
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
-        public int Name { get; set; }
+        public string? Name { get; set; }
     }
 }
